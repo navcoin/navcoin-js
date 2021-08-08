ef6b04dadd1e6f9aa3f3cedd3239b8dde518b295
alex v <alex@encrypt-s.com>
2021-08-08 15:21:34 +0200

    update

--- example.js
+++ example.js
@@ -3,9 +3,15 @@ const njs = require('./index');
 const walletFile = 'wallet.db'; // File name of the wallet database
 const password = undefined; // Password used to encrypt and open the wallet database
 const spendingPassword = undefined; // Password used to send transactions
+const mnemonic = undefined; // Mnemonic to import
+const type = undefined; // Wallet type next or navcoin-js-v1
+const zapwallettxes = false; // Should the wallet be cleared of its history?
+const log = true; // Log to console
+
+let alreadySent = false;
 
 njs.wallet.Init().then(async () => {
-    const wallet = new njs.wallet.WalletFile({file: walletFile, password: password, spendingPassword: spendingPassword})
+    const wallet = new njs.wallet.WalletFile({file: walletFile, mnemonic: mnemonic, type: type, password: password, spendingPassword: spendingPassword, zapwallettxes: zapwallettxes, log: log})
 
     wallet.on('new_mnemonic', (mnemonic) => console.log(`wallet created with mnemonic ${mnemonic} - please back it up!`));
 
@@ -20,8 +26,13 @@ njs.wallet.Init().then(async () => {
 
     wallet.on('connected', () => console.log('connected. waiting for sync'));
 
-    wallet.on('sync_status', (progress) => {
+    wallet.on('sync_status', async (progress) => {
         console.log(`Sync ${progress}%`)
+        if (progress == 100 && !alreadySent) {
+                 let txHash = await wallet.xNavSend('xNTpkPKgypNCsJmVk8PoNomqcmYEBHyHgZVjCPpFb2CbAv3ZsxmdFGfMJh7DL8U2JvkQULxj1kUHp8ATuhAsG3z2woufMcbyDT8doU2djJibaBzp3UFu2CKg2rsVD5fih2TaPzrwi9X', 1e8, 'memo for xnav payment', spendingPassword);
+       console.log(`Transaction sent to xN1231.... with hash ${txHash}`);
+          alreadySent = true;
+        }
     });
 
     wallet.on('new_tx', async (list) => {
