5d5b52282412f867f37cdfbc9895a7b31f6a1b97
alex v <alex@encrypt-s.com>
2021-09-26 21:53:41 +0200

    update example.js

--- example.js
+++ example.js
@@ -1,67 +1,125 @@
-const njs = require('./index');
+require("fake-indexeddb/auto");
+const njs = require("./index");
 
-const walletFile = 'wallet-r.db'; // File name of the wallet database
+const walletFile = undefined; // File name of the wallet database
 const password = undefined; // Password used to encrypt and open the wallet database
 const spendingPassword = undefined; // Password used to send transactions
-const mnemonic = undefined; // Mnemonic to import
+const mnemonic = undefined; // Mnemonic to import 'problem shrimp bottom mouse canyon moment dirt beyond cage hazard phrase animal';
 const type = undefined; // Wallet type next, navcoin-core or navcoin-js-v1
 const zapwallettxes = false; // Should the wallet be cleared of its history?
 const log = true; // Log to console
+const network = "mainnet";
 
 let alreadySent = false;
 
 njs.wallet.Init().then(async () => {
-    const wallet = new njs.wallet.WalletFile({file: walletFile, mnemonic: mnemonic, type: type, password: password, spendingPassword: spendingPassword, zapwallettxes: zapwallettxes, log: log})
+  console.log({
+    file: walletFile,
+    mnemonic: mnemonic,
+    type: type,
+    password: password,
+    spendingPassword: spendingPassword,
+    zapwallettxes: zapwallettxes,
+    log: log,
+    network: network,
+  });
+  const wallet = new njs.wallet.WalletFile({
+    file: walletFile,
+    mnemonic: mnemonic,
+    type: type,
+    password: password,
+    spendingPassword: spendingPassword,
+    zapwallettxes: zapwallettxes,
+    log: log,
+    network: network,
+  });
 
-    wallet.on('new_mnemonic', (mnemonic) => console.log(`wallet created with mnemonic ${mnemonic} - please back it up!`));
+  wallet.on("new_mnemonic", (mnemonic) =>
+    console.log(`wallet created with mnemonic ${mnemonic} - please back it up!`)
+  );
 
-    wallet.on('loaded', async () => {
-        console.log('wallet loaded')
+  wallet.on("loaded", async () => {
+    console.log("wallet loaded");
 
-        console.log('xNAV receiving address: '+ (await wallet.xNavReceivingAddresses(false))[0].address);
-        console.log('NAV receiving address: '+ (await wallet.NavReceivingAddresses(false))[0].address);
+    console.log(
+      "xNAV receiving address: " +
+        (await wallet.xNavReceivingAddresses(true))[0].address
+    );
+    console.log(
+      "NAV receiving address: " +
+        (await wallet.NavReceivingAddresses(true))[0].address
+    );
 
-        let pk = (await wallet.NavGetPrivateKeys())[0].privateKey;
+    let pk = (await wallet.NavGetPrivateKeys())[0].privateKey;
 
-        let sig = wallet.Sign(pk, "hola")
-        console.log(wallet.VerifySignature((await wallet.NavGetPrivateKeys())[0].address, "hola", sig))
+    let sig = wallet.Sign(pk, "hola");
+    console.log(
+      wallet.VerifySignature(
+        (await wallet.NavGetPrivateKeys())[0].address,
+        "hola",
+        sig
+      )
+    );
 
-        await wallet.Connect();
-    });
+    await wallet.Connect();
+  });
 
-    wallet.on('connected', () => console.log('connected. waiting for sync'));
+  wallet.on("connected", () => console.log("connected. waiting for sync"));
 
-    wallet.on('sync_status', async (progress, scripthash) => {
-        console.log(`Sync ${progress}%`)
-        if (progress == 100 && scripthash == "6032c38c0bc0e91e726f1e55e1832e434509001a7aed5cfd881b6ef07215e84a") {
-            if (!alreadySent)
-            {
-                //let txHash = await wallet.xNavSend((await wallet.xNavReceivingAddresses(false))[0].address, 1e6, 'memo for xnav payment', spendingPassword);
-                //console.log(`Transaction sent to ${(await wallet.xNavReceivingAddresses(false))[0].address}.... with hash ${txHash}`);
-                alreadySent = true;
-            }
-            console.log(await wallet.GetHistory())
-        }
-    });
+  wallet.on("sync_status", async (progress, pending) => {
+    console.log(`Sync ${progress}%`);
+    if (progress == 100 && !alreadySent) {
+      alreadySent = true;
+      if ((await wallet.GetBalance()).xnav.confirmed > 0) {
+        let tx = await wallet.xNavCreateTransaction(
+          (
+            await wallet.xNavReceivingAddresses(true)
+          )[0].address,
+          1e6,
+          "memo for xnav payment",
+          spendingPassword
+        );
+        /*let hash = await wallet.SendTransaction(tx.tx)
+                console.log(`Transaction sent to ${(await wallet.xNavReceivingAddresses(false))[0].address}.... with hash ${hash}`);*/
+      }
 
-    wallet.on('new_tx', async (list) => {
-        console.log(`Received transaction ${JSON.stringify(list)}`)
-        console.log(`Balance ${JSON.stringify(await wallet.GetBalance())}`)
-    });
+      if ((await wallet.GetBalance()).nav.confirmed > 0) {
+        let tx = await wallet.NavCreateTransaction(
+          (
+            await wallet.NavReceivingAddresses(true)
+          )[0].address,
+          1e6,
+          undefined,
+          spendingPassword
+        );
+        /*let hash = await wallet.SendTransaction(tx.tx)
+                console.log(`Transaction sent to ${(await wallet.NavReceivingAddresses(false))[0].address}.... with hash ${hash}`);*/
+      }
+    }
+  });
 
-    wallet.on('remove_tx', async (txid) => {
-        console.log(`Removed tx transaction ${txid}`)
-    });
+  wallet.on("sync_finished", async () => {
+    console.log("sync_finished");
+    console.log(await wallet.GetBalance());
+  });
 
-    await wallet.Load();
+  wallet.on("new_tx", async (list) => {
+    console.log(`Received transaction ${JSON.stringify(list)}`);
+    console.log(`Balance ${JSON.stringify(await wallet.GetBalance())}`);
+  });
 
-    // console.log(await wallet.GetHistory());
-    // console.log(`Last block: ${await wallet.GetTip()}`);
+  wallet.on("remove_tx", async (txid) => {
+    console.log(`Removed tx transaction ${txid}`);
+  });
 
-    // let txHash = await wallet.xNavSend('xN123124fa123123123212131231', 10*1e8, 'memo for xnav payment', spendingPassword);
-    // console.log(`Transaction sent to xN1231.... with hash ${txHash}`);
+  await wallet.Load({ bootstrap: njs.wallet.xNavBootstrap });
 
-    // txHash = await wallet.xNavSend('N123124fa123121', 10*1e8, '', spendingPassword);
-    // console.log(`Transaction sent to N1231.... with hash ${txHash}`);
+  // console.log(await wallet.GetHistory());
+  // console.log(`Last block: ${await wallet.GetTip()}`);
 
+  // let txHash = await wallet.xNavSend('xN123124fa123123123212131231', 10*1e8, 'memo for xnav payment', spendingPassword);
+  // console.log(`Transaction sent to xN1231.... with hash ${txHash}`);
+
+  // txHash = await wallet.xNavSend('N123124fa123121', 10*1e8, '', spendingPassword);
+  // console.log(`Transaction sent to N1231.... with hash ${txHash}`);
 });
