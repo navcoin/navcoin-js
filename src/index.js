90e84fbbdb9e86f364b4cf8b9365c70bb4039553
alex <alex@nav.community>
2022-04-25 15:53:03 +0200

    Release 1.1.96

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-export var version = "1.1.95";export * as wallet from "./lib/wallet.js";
+export var version = "1.1.96";export * as wallet from "./lib/wallet.js";
--- src/lib/wallet.js
+++ src/lib/wallet.js
@@ -271,7 +271,7 @@ export class WalletFile extends events.EventEmitter {
 
     if (!mk) return;
 
-    while ((await this.GetPoolSize(AddressTypes.XNAV)) < 5) {
+    while ((await this.GetPoolSize(AddressTypes.XNAV)) < 3) {
       await this.xNavCreateSubaddress(spendingPassword);
     }
   }
@@ -283,7 +283,7 @@ export class WalletFile extends events.EventEmitter {
 
     if (!mk) return;
 
-    while ((await this.GetPoolSize(AddressTypes.NAV)) < 5) {
+    while ((await this.GetPoolSize(AddressTypes.NAV)) < 3) {
       await this.NavCreateAddress(spendingPassword);
     }
   }
